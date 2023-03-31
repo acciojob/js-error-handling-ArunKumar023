48210b0ea876e48710e42beaab7e5be32ffcb787
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-03-31 07:13:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
 class OutOfRangeError extends Error {
   constructor(arg) {
     super(`Expression should only consist of integers and +-/* characters and not ${arg}`);
-    this.name = this.constructor.name;
+    this.name = 'OutOfRangeError';
   }
 }
 
 class InvalidExprError extends Error {
   constructor() {
-    super(`Expression should not have an invalid combination of expression`);
-    this.name = this.constructor.name;
+    super('Expression should not have an invalid combination of expression');
+    this.name = 'InvalidExprError';
   }
 }
 
 function evalString(str) {
   try {
-    if (/^[+*/]/.test(str)) {
+    if (/^\s*[+\-/*]/.test(str)) {
       throw new SyntaxError('Expression should not start with invalid operator');
     }
-    if (/[\+\-\*\/][\+\*\/]$/.test(str)) {
-      throw new InvalidExprError();
-    }
-    if (/[+*/-]$/.test(str)) {
+    if (/[+\-/*]\s*$/.test(str)) {
       throw new SyntaxError('Expression should not end with invalid operator');
     }
-    const regex = /[-+]?[0-9]+([*\/+][-+]?[0-9]+)*/g;
-    const matches = str.match(regex);
-    if (!matches || matches.join("") !== str) {
-      throw new OutOfRangeError(str);
+    if (/([+\-/*]){2,}/.test(str)) {
+      throw new InvalidExprError();
     }
-    return eval(str);
+    const result = eval(str);
+    if (!Number.isInteger(result)) {
+      throw new OutOfRangeError(`result: ${result}`);
+    }
+    return result;
   } catch (error) {
-    console.error(error);
+    if (error instanceof OutOfRangeError || error instanceof InvalidExprError) {
+      throw error;
+    }
+    throw new OutOfRangeError(error.message);
   }
 }
-
-// Test cases
-console.log(evalString("1 + 2 - 3")); // Output: 0
-console.log(evalString("1 + + 2")); // Output: SyntaxError: Expression should not have an invalid combination of expression
-console.log(evalString("*1 + 2 - 3*")); // Output: OutOfRangeError: Expression should only consist of integers and +-/* characters and not *1 + 2 - 3*
-console.log(evalString("1 / 0")); // Output: Infinity
-console.log(evalString("-1 * 2 + 3")); // Output: 1
