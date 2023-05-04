6a82416bdf7f19525ede35e6f2c2208cf43342a5
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-05-04 10:38:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,52 +1,40 @@
 class OutOfRangeError extends Error {
   constructor(arg) {
     super(`Expression should only consist of integers and +-/* characters and not ${arg}`);
-    this.name = "OutOfRangeError";
+    this.name = this.constructor.name;
   }
 }
 
-// InvalidExprError class
 class InvalidExprError extends Error {
   constructor() {
-    super(`Expression should not have an invalid combination of expression`);
-    this.name = "InvalidExprError";
+    super('Expression should not have an invalid combination of expression');
+    this.name = this.constructor.name;
   }
 }
 
-// evalString function
-function evalString(str) {
-  // Check for invalid combinations of operators
-  if (str.match(/[+\-*/]{2}/)) {
+function evalString(expr) {
+  if (/[\+\-\*\/]{2,}/.test(expr)) {
     throw new InvalidExprError();
   }
 
-  // Check for invalid starting operators
-  if (str.match(/^[+\-*/]/)) {
-    throw new SyntaxError("Expression should not start with invalid operator");
+  if (/^[\+\*\/]/.test(expr)) {
+    throw new SyntaxError('Expression should not start with invalid operator');
   }
 
-  // Check for invalid ending operators
-  if (str.match(/[+\-*/]$/)) {
-    throw new SyntaxError("Expression should not end with invalid operator");
+  if (/[\+\*\/\-]$/.test(expr)) {
+    throw new SyntaxError('Expression should not end with invalid operator');
   }
 
-  // Check for other invalid characters
-  if (str.match(/[^0-9+\-*/\s]/)) {
-    let invalidChar = str.match(/[^0-9+\-*/\s]/)[0];
-    throw new OutOfRangeError(invalidChar);
+  if (/[^0-9+\-*/\s]/.test(expr)) {
+    throw new OutOfRangeError(expr.match(/[^0-9+\-*/\s]/)[0]);
   }
 
-  // Evaluate the expression and return the result
-  return eval(str);
+  return eval(expr);
 }
 
-// Testing the evalString function
 try {
-  let result = evalString("3 + 4 * 2");
-  console.log(result); // Output: 11
-
-  result = evalString("2 / 0");
-  console.log(result); // This line won't execute due to the thrown error
-} catch (err) {
-  console.log(err.name + ": " + err.message);
+  const result = evalString('1 + 2 * 3');
+  console.log(result); // 7
+} catch (e) {
+  console.error(e);
 }
